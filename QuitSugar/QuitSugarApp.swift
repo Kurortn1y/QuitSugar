800335cd1b0aaafd52127b5b6d3a3dea186a01bc
RomaRoma <lastmandanc1ng@icloud.com>
2024-03-28 00:35:31 +0100

    Initial Commit

---
+++ QuitSugar/QuitSugarApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitSugarApp.swift
+//  QuitSugar
+//
+//  Created by Roman on 28.03.24.
+//
+
+import SwiftUI
+
+@main
+struct QuitSugarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
